59df80bb4572b6de6243fd8e03bcc7e65ac5f459
rasjidzz <mrisjads@gmail.com>
2023-10-12 15:15:58 +0700

    Upd : Landing Page

--- app/Views/Modules/Landing_Page/index.php
+++ app/Views/Modules/Landing_Page/index.php
@@ -1,15 +1,64 @@
-<?= $this->extend('layout/template'); ?>
+<?= $this->extend('layout/template_nonAuth'); ?>
 
 <?= $this->section('content'); ?>
 
-<div class="container position-relative rounded-3 " style=" display:flex; justify-content: center;  background-image: url('/Assets/GKU.jpg'); background-size: cover; height: 80vh; font-family: 'Open Sans', sans-serif;">
-    <div class="overlay rounded-3">
-        <div class="m-5" style="text-align: center; color: white;">
-            <h1>SELAMAT DATANG, MAU SEWA FASILITAS ?</h1>
-            <h1>MASUK PAKAI AKUN SSO DULU YUK ?</h1>
+
+
+<!-- <section class="mt-5">
+    <div class="container">
+        <h1>test</h1>
+
+    </div>
+</section> -->
+
+<section class="mt-5">
+
+    <div class="container position-relative rounded-3 " style=" display:flex; justify-content: center;  background-image: url('/Assets/GKU.jpg'); background-size: cover; height: 70vh; font-family: 'Open Sans', sans-serif;">
+        <div class="overlay rounded-3">
+            <div class="row">
+                <div class="mt-5" style="text-align: center; color: white;">
+                    <h1 style="font-weight: bold; color: white;">SELAMAT DATANG, MAU SEWA FASILITAS ?</h1>
+                    <h1 style="font-weight: bold; color: white;">MASUK PAKAI AKUN SSO DULU YUK ?</h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="mt-5" style="text-align: center;">
+                    <h1 style="color: white;">Sign In</h1>
+                </div>
+                <div class="mt-3" style="text-align: center;">
+                    <!-- <button class="btn btn-secondary">Connect with office 365</button> -->
+                    <button class="btn btn-light" style="width: 400px;">
+                        <img src="/Assets/365Logo.png" alt="Office 365 Logo" style="max-width: 20px; margin-right: 5px;">
+                        Connect with Office 365
+                    </button>
+                </div>
+            </div>
+            <div class="row mt-5">
+                <div class="col" style="text-align: center; color: white;">adsadaa</div>
+                <div class="col-6" style="text-align: center;">
+                    <div class="container" style="background-color: #f0f0f0; padding: 20px;">
+                        <p class="fw-normal" style="font-weight: bold">
+                            Jenis Fasilitas Yang Bisa Anda Sewa
+                        </p>
+                        <div class="row">
+                            <div class="col">
+                                Gedung
+                            </div>
+                            <div class="col">
+                                Kelas
+                            </div>
+                            <div class="col">
+                                Olahraga
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col" style="text-align: center; color: white">asdadasd</div>
+            </div>
         </div>
     </div>
-</div>
+</section>
+
 <style>
     @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap');
 
@@ -21,9 +70,7 @@
         width: 100%;
         height: 100%;
         background: rgba(0, 0, 0, 0.5);
-        /* Warna latar belakang hitam dengan tingkat kecerahan 0.5 (50%) */
         z-index: 0;
-        /* Z-index harus lebih rendah daripada konten agar berada di belakangnya */
     }
 </style>
 
--- app/Views/layout/template_nonAuth.php
+++ app/Views/layout/template_nonAuth.php
@@ -59,14 +59,14 @@
         </div>
     </footer> -->
     <footer class="bg-dark p-3 fixed-bottom">
-        <div class="container">
+        <div class="container-fluid">
             <div class="row d-flex align-items-center">
                 <div class="col-md-6 text-center">
-                    <img src="/Assets/Logo_Rentit.png" alt="Company Logo" class="img-fluid" style="max-width: 100px; height: auto;">
+                    <img src="/Assets/Logo_Rentit.png" alt="Company Logo" class="img-fluid" style="max-width: 120px; height: auto;">
                 </div>
                 <div class="col-md-6 text-center">
                     <ul class="list-inline">
-                        <li class="list-inline-item"><a href="#"><img src="/Assets/face.png" alt="" class="img-fluid" style="max-width: 30px; height: auto;"></a></li>
+                        <li class="list-inline-item"><a href="https://www.facebook.com/telkomuniversity"><img src="/Assets/face.png" alt="" class="img-fluid" style="max-width: 30px; height: auto;"></a></li>
                         <li class="list-inline-item"><a href="https://www.instagram.com/telkomuniversity/"><img src="/Assets/insta.png" alt="" class="img-fluid" style="max-width: 30px; height: auto;"></a></li>
                         <li class="list-inline-item"><a href="https://www.linkedin.com/school/telkom-university/"><img src="/Assets/Linked.png" alt="" class="img-fluid" style="max-width: 30px; height: auto;"></a></li>
                         <li class="list-inline-item"><a href="https://twitter.com/TelUniversity"><img src="/Assets/twit.png" alt="" class="img-fluid" style="max-width: 30px; height: auto;"></a></li>
