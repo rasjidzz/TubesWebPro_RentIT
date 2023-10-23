6499c27260759a06dd3786c5596109f85d30aea8
logicrius <113976617+logicrius@users.noreply.github.com>
2023-10-23 18:24:22 +0700

    commit RentPage FE - 23/10/2023
    frontEndnya rentpage

--- app/Views/layout/template_nonAuth.php
+++ app/Views/layout/template_nonAuth.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $title; ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font@7.2.96/css/materialdesignicons.min.css">
 </head>
 
 <style>
@@ -15,8 +16,8 @@
 </style>
 
 <body>
-    <div class="text-white py-2 pl-2" style="background-color: #b6252a;"></div>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="text-white py-2 pl-2 sticky-top" style="background-color: #b6252a;"></div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top ">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">
                 <img src="/Assets/Logo_Rentit.png" style="max-width: 100px; height: auto;">
@@ -45,7 +46,7 @@
 
     <!-- Ambil Footer -->
 
-    <footer class="bg-dark p-3 fixed-bottom">
+    <footer class="bg-dark p-3 ">
         <div class="container-fluid">
             <div class="row d-flex align-items-center">
                 <div class="col-md-6 text-center">
