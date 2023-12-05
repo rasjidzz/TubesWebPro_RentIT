e2e9aa367c9479a6a556872b56882f9b03d4d373
rasjidzz <mrisjads@gmail.com>
2023-12-05 11:48:25 +0700

    Upd : Otw Migration to Laravel

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -6,8 +6,8 @@ use CodeIgniter\Config\BaseConfig;
 
 class Email extends BaseConfig
 {
-    public string $fromEmail  = '';
-    public string $fromName   = '';
+    public string $fromEmail  = 'rent.it.demo.2023@gmail.com';
+    public string $fromName   = 'Rent It Demo';
     public string $recipients = '';
 
     /**
